61a3b535e49ddbfcc2be9d4a8b24cfbe8b57cffe
Sajad Mirzaei <sajadmirz4ei@gmail.com>
2024-05-28 20:26:48 +0330

    User validation added

--- OpaAuth/Models/User.cs
+++ OpaAuth/Models/User.cs
@@ -4,8 +4,14 @@ namespace OpaAuth.Models
 {
     public class User
     {
+        [MaxLength(50)]
+        [Required]
         public string Name { get; set; }
+        [EmailAddress]
+        [Required]
         public string Email { get; set; }
+        [MaxLength(50)]
+        [Required]
         public string Role { get; set; }
     }
 }
